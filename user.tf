d95f144c1e8c34f662bf07905f6c2e75cad60342
bhaiyapatil6605 <156217059+bhaiyapatil6605@users.noreply.github.com>
2024-02-12 14:52:25 +0530

    Create user.tf

---
+++ user.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_iam_user" "ashu_user" {
+  name = "ashu"  # Specify the username for the IAM user
+  
+  tags = {
+    Name = "Ashu User"  # Add tags for the IAM user if needed
+  }
+}
