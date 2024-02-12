5d23c4cc9e18535f1f04bcd71f19600ee72acae7
bhaiyapatil6605 <156217059+bhaiyapatil6605@users.noreply.github.com>
2024-02-12 14:31:30 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance" {
-    ami                 = "(ami-0702b1aec03c6a3a4)"
+    ami                 = "ami-0702b1aec03c6a3a4"
     availability_zone   = "ap-southeast-1"
     instance_type       = "t2.micro"
     key_name            = "singapore.akey"
