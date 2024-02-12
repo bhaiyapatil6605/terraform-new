6d0a0baff4ce709d6ecca9fc4aa8929fb0390a4d
bhaiyapatil6605 <156217059+bhaiyapatil6605@users.noreply.github.com>
2024-02-12 15:01:09 +0530

    Update user.tf

--- user.tf
+++ user.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 resource "aws_iam_user" "ashu_user" {
   name = "ashu"  # Specify the username for the IAM user
+  path = "/alias/"  # Specify the alias for the IAM user
   
   tags = {
     Name = "Ashu User"  # Add tags for the IAM user if needed
